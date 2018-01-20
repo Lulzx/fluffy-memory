ce135e2dc88acdc4e650f15892ecb8799fc73fea
Lulzx <wiserishabh@gmail.com>
2018-01-20 11:11:00 +0000

    Added tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const newer = require('gulp-newer');
 
+const src = 'src/**/*.js';
+const dest = 'dist';
+
+gulp.task('babel', () => {
+  return gulp.src([src])
+    .pipe(newer(dest))
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(src, ['babel']);
+});
+
+gulp.task('default', ['babel']);

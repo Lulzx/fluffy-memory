eeff79a69884a6ff04bbba67db3e26335e5db100
lulzx <wiserishabh@gmail.com>
2018-01-20 18:28:09 +0530

    lol

--- src/commands.js
+++ src/commands.js
@@ -1,33 +1,42 @@
 const search = {
-    regex: /\/search\s(\w+)\s(.+)/,
-    inlineRegex: /(\w+)\s(.+)/,
-    help: ``
+  regex: /\/search\s(\w+)\s(.+)/,
+  inlineRegex: /(\w+)\s(.+)/,
+  help: ``
 };
+ 
 const help = {
-    regex: /\/help/,
-    initMessage: `I can search *users*, *repos* and *issues* for you.
-
+  regex: /\/help/,
+  initMessage: `I can search *users*, *repos* and 
+*issues* for you.
+ 
 *users*:
   /search u user
-
+ 
 *repos*:
   /search r repo
-
+ 
 *issues*:
   /search i issue
-
-Note that you can use _user_, _users_, _repo_, _repos_, ` + `_issue_ or _issues_ instead of that _u_, _r_, or _i_.
-Those are just some simple shortcuts.,
-    help: ` `
+ 
+Note that you can use _user_, _users_, _repo_, 
+_repos_, ` + `_issue_ or _issues_ instead of that 
+_u_, _r_, or _i_. Those are just some simple 
+shortcuts. `,
+  help: ``
 };
+ 
 const start = {
-    regex: /\/start/,
-    initMessage: ` Heyo !I 'm here to help you in order to find *repositories, users and issues* from GitHub' +
-            ' directly in Telegram.\nSend /help to know how to use me.`'
+  regex: /\/start/,
+  initMessage: `Heyo! I am here to help you in order 
+to find *repositories, users and issues* from GitHub 
+directly in Telegram. Send /help to know how to use 
+me.`
 };
+ 
 const commands = {
-    search,
-    help,
-    start
+  search,
+  help,
+  start
 };
+ 
 export default commands;

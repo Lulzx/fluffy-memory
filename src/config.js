17055a3fb7eb87f6c41744d7c9ebd9ce9f800472
Lulzx <wiserishabh@gmail.com>
2018-06-13 08:09:09 +0530

    Updated.

--- src/config.js
+++ src/config.js
@@ -1,13 +1,11 @@
 const token = 'BOT_TOKEN';
-const github = {
-    version: '13.1.1',
-    debug: true,
-    protocol: 'https',
-    host: 'api.github.com',
-    pathPrefix: '',
+const octokit = require('@octokit/rest')({
     timeout: 5000,
     headers: {
-        'user-agent': 'fluffy memory'
-    }
-};
-export default {token, github};
+        accept: 'application/vnd.github.v3+json',
+        'user-agent': 'octokit/rest.js v1.2.3'
+    },
+    baseUrl: 'https://api.github.com',
+    agent: undefined
+});
+export default {token, octokit};
